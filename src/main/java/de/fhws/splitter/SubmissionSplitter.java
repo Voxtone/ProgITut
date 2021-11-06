09330d91509e7c115867e65ac255d747adbc452d
Dave <davecraft270@gmail.com>
2021-11-06 20:47:04 +0100

    Cleaned up SubmissionSplitter

--- src/main/java/de/fhws/splitter/SubmissionSplitter.java
+++ src/main/java/de/fhws/splitter/SubmissionSplitter.java
@@ -1,10 +1,10 @@
 package de.fhws.splitter;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.zip.ZipException;
 
 import de.fhws.excpetions.MultipleFilesContainedException;
 import de.fhws.excpetions.NoFileContainedException;
@@ -13,17 +13,24 @@ import de.fhws.excpetions.NotAnArchiveException;
 import org.rauschig.jarchivelib.Archiver;
 import org.rauschig.jarchivelib.ArchiverFactory;
 
+// TODO check all amount cases
+
+
 public class SubmissionSplitter {
-    public static final String SUBMISSIONS_DIR = "files/submissions/";
-    public static final String OUTPUT_DIR = "files/split/";
-    public static final int TOM_AMOUNT = 10;
-    public static final int DAVE_AMOUNT = 30;
-    public static final int NICO_AMOUNT = 20;
-    public static final int NICO2_AMOUNT = 20;
+    public static final String SUBMISSIONS_PATH = "files/submissions";
+    public static final String OUTPUT_PATH = "files/split";
+    public static final int TOM_AMOUNT = 18;
+    public static final int DAVE_AMOUNT = 38;
+    public static final int NICOLAS_AMOUNT = 34;
+    public static final int JOHANNES_AMOUNT = 21;
+
+    // paths
+    private final File submissions;
+    private final File outputDir;
 
     // for logic
-    private Map<String, Integer> amountMap;
-    private Map<String, List<File>> fileMap = new HashMap<>();
+    private final Map<String, Integer> amountMap;
+    private final Map<String, List<File>> fileMap = new HashMap<>();
     private int summedAmount;
 
     // Archiver
@@ -31,18 +38,38 @@ public class SubmissionSplitter {
     private static final Archiver tar = ArchiverFactory.createArchiver("tar");
     private static final Archiver targz = ArchiverFactory.createArchiver("tar", "gz");
 
-    public SubmissionSplitter(Map<String, Integer> amountsMap) {
+    /**
+     * Constructs a SubmissionSplitter with the given amounts map
+     * @param amountsMap map which describes who has how many submissions per week according to the contract
+     * @param submissionsPath path to the submissions
+     * @param outputPath path to the output directory
+     */
+    public SubmissionSplitter(Map<String, Integer> amountsMap, String submissionsPath, String outputPath) {
         this.amountMap = amountsMap;
         summedAmount = 0;
         for (String name : amountsMap.keySet()) {
             summedAmount += amountsMap.get(name);
             fileMap.put(name, new ArrayList<>());
         }
+        submissions = new File(submissionsPath);
+        outputDir = new File(outputPath);
+        for(File f : outputDir.listFiles())
+            recursiveDelete(f);
 
     }
 
+    private boolean recursiveDelete(File file) {
+        if(file.isDirectory()) {
+            for(File f : file.listFiles())
+                recursiveDelete(f);
+        }
+        return file.delete();
+    }
+
+    /**
+     * splits the submissions in the specified output directory into the
+     */
     public void split() {
-        File submissions = new File(SUBMISSIONS_DIR);
         List<File> allFiles = Arrays.stream(submissions.listFiles()).collect(Collectors.toList());
         int fileCount = allFiles.size();
         int overhead = (fileCount - summedAmount) / amountMap.size();
@@ -54,6 +81,7 @@ public class SubmissionSplitter {
             if (remainder-- > 0)
                 giveFileTo(name, allFiles.remove(0));
         }
+
         printSplit(fileCount);
         executeSplit();
     }
@@ -82,8 +110,8 @@ public class SubmissionSplitter {
                         throw new NoFileContainedException(dir.getName());
                     if (dir.listFiles().length != 1)
                         throw new MultipleFilesContainedException(dir.getName());
-                    System.out.println(extractName(dir));
-                    extractArchiveTo(dir.listFiles()[0], new File(OUTPUT_DIR + name + "/" + extractName(dir)));
+
+                    extractArchiveTo(dir.listFiles()[0], new File(outputDir.getPath() + "/" + name + "/" + extractName(dir)));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -111,6 +139,12 @@ public class SubmissionSplitter {
                 throw new NotAnArchiveException(source.getName());
 
             archiver.extract(source, dest);
+        } catch (ZipException e) {
+            try {
+                throw new NotAnArchiveException(source.getPath());
+            } catch (NotAnArchiveException notAnArchiveException) {
+                notAnArchiveException.printStackTrace();
+            }
         } catch (NotAnArchiveException | IOException e) {
             e.printStackTrace();
         }
@@ -124,9 +158,9 @@ public class SubmissionSplitter {
         Map<String, Integer> amountMap = new HashMap<>();
         amountMap.put("TOM", TOM_AMOUNT);
         amountMap.put("DAVE", DAVE_AMOUNT);
-        //  amountsMap.put("NICO", NICO_AMOUNT);
-        //  amountsMap.put("NICO2", NICO2_AMOUNT);
-        SubmissionSplitter splitter = new SubmissionSplitter(amountMap);
+        //amountMap.put("NICOLAS", NICOLAS_AMOUNT);
+        //amountMap.put("JOHANNES", JOHANNES_AMOUNT);
+        SubmissionSplitter splitter = new SubmissionSplitter(amountMap, SUBMISSIONS_PATH, OUTPUT_PATH);
         splitter.split();
     }
 
